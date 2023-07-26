d080b7e126bc561e7fb99cb609a2fca0ffd948ae
GoGoWen <GoGoWen2021@gmail.com>
2023-07-26 17:18:08 +0800

    fix date type error

--- fe/fe-core/src/main/java/org/apache/doris/external/iceberg/util/IcebergUtils.java
+++ fe/fe-core/src/main/java/org/apache/doris/external/iceberg/util/IcebergUtils.java
@@ -341,9 +341,7 @@ public class IcebergUtils {
             } catch (ParseException e) {
                 return null;
             }
-            return String.format("%04d-%02d-%02d %02d:%02d:%02d", dateLiteral.getYear(),
-                dateLiteral.getMonth(), dateLiteral.getDay(), dateLiteral.getHour(), dateLiteral.getMinute(),
-                dateLiteral.getMinute());
+            return String.format("%04d-%02d-%02d", dateLiteral.getYear(), dateLiteral.getMonth(), dateLiteral.getDay());
         } else if (expr instanceof DecimalLiteral) {
             DecimalLiteral decimalLiteral = (DecimalLiteral) expr;
             return decimalLiteral.getValue();
