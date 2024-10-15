47e14093b530001976aaf5c2383fef69a79a1683
zhiqiang <seuhezhiqiang@163.com>
2024-10-15 17:15:05 +0800

    [fix](cancel) Fix cancel msg on branch-2.1 (#41798)
    Make sure we can tell cancel reason from:
    1. user cancel
    2. timeout
    3. others
    
    ```text
    mysql [demo]>set query_timeout=1;
    --------------
    set query_timeout=1
    --------------
    
    Query OK, 0 rows affected (0.00 sec)
    
    mysql [demo]>select sleep(5);
    --------------
    select sleep(5)
    --------------
    
    ERROR 1105 (HY000): errCode = 2, detailMessage = Timeout
    
    mysql [demo]>select sleep(5);
    --------------
    select sleep(5)
    --------------
    
    ^C^C -- sending "KILL QUERY 0" to server ...
    ^C -- query aborted
    ERROR 1105 (HY000): errCode = 2, detailMessage = cancel query by user from 127.0.0.1:64208
    ```
    
    Conflicts:
    	fe/fe-core/src/main/java/org/apache/doris/qe/StmtExecutor.java

--- fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/job/extensions/mtmv/MTMVTask.java
@@ -266,7 +266,7 @@ public class MTMVTask extends AbstractTask {
     protected synchronized void executeCancelLogic() {
         LOG.info("mtmv task cancel, taskId: {}", super.getTaskId());
         if (executor != null) {
-            executor.cancel();
+            executor.cancel("mtmv task cancelled");
         }
         after();
     }
