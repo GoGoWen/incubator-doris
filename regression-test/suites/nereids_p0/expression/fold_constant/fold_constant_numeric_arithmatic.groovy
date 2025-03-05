0134ab04b804c4991ce10952297fad838c169325
zhangsiqi62 <zhangsiqi62@jd.com>
2025-03-05 11:41:22 +0800

    fix(Nereids): divide const folding for decimalv3 return type is not correct

--- regression-test/suites/nereids_p0/expression/fold_constant/fold_constant_numeric_arithmatic.groovy
+++ regression-test/suites/nereids_p0/expression/fold_constant/fold_constant_numeric_arithmatic.groovy
@@ -348,6 +348,10 @@ suite("fold_constant_numeric_arithmatic") {
     testFoldConst("SELECT 5 ^ 3 AS xor_case_1") //5 XOR 3 = 6
     testFoldConst("SELECT 0 ^ 1 AS xor_case_2") //0 XOR 1 = 1
     testFoldConst("SELECT 255 ^ 128 AS xor_case_3") //255 XOR 128
+
+    // ensure divide for decimal v3 could return correct type when divider is 0
+    sql """ select if(random() > 0.5, cast(random() as decimal(38,10)), cast(0 as decimal(30, 10)) / cast(0 as decimal(30,10)))"""
+
 //Exception: XOR on non-integer types (if applicable)
 test {
     sql "SELECT 'a' ^ 1 AS xor_case_exception"
