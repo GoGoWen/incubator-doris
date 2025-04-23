73c06fda4b61d054f1751f769f197c4525f28580
caiconghui1 <caiconghui1@jd.com>
2025-04-23 14:39:11 +0800

    enhancement(partition): support get conjuncts without partition predicate for hms table (#467)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/glue/translator/PhysicalPlanTranslator.java
@@ -76,6 +76,7 @@ import org.apache.doris.nereids.properties.DistributionSpecTableSinkRandomPartit
 import org.apache.doris.nereids.properties.DistributionSpecTabletIdShuffle;
 import org.apache.doris.nereids.properties.OrderKey;
 import org.apache.doris.nereids.properties.PhysicalProperties;
+import org.apache.doris.nereids.rules.expression.rules.PartitionPruneExpressionExtractor;
 import org.apache.doris.nereids.rules.implementation.LogicalWindowToPhysicalWindow.WindowFrameGroup;
 import org.apache.doris.nereids.rules.rewrite.MergeLimits;
 import org.apache.doris.nereids.stats.StatsErrorEstimator;
@@ -218,6 +219,7 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -665,12 +667,39 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
         return getPlanFragmentForPhysicalFileScan(fileScan, context, scanNode, table, tupleDescriptor);
     }
 
+    /**
+     *  get conjuncts without partition predicate if file scan table is hms table
+     *
+     */
+    public Set<Expression> getConjunctsWithoutPartitionPredicate(PhysicalFileScan fileScan) {
+        if (fileScan.getTable() instanceof HMSExternalTable) {
+            Map<String, Slot> scanOutput = fileScan.getOutput()
+                    .stream()
+                    .collect(Collectors.toMap(slot -> slot.getName().toLowerCase(), Function.identity()));
+            Set<Slot> partitionSlots = ((HMSExternalTable) fileScan.getTable()).getPartitionColumns()
+                    .stream()
+                    .map(column -> scanOutput.get(column.getName().toLowerCase()))
+                    .collect(Collectors.toSet());
+            PartitionPruneExpressionExtractor.ExpressionEvaluableDetector detector =
+                    new PartitionPruneExpressionExtractor.ExpressionEvaluableDetector(partitionSlots);
+            Set<Expression> result = Sets.newHashSet();
+            for (Expression expression : fileScan.getConjuncts()) {
+                if (!detector.detect(expression)) {
+                    result.add(expression);
+                }
+            }
+            return result;
+        } else {
+            return fileScan.getConjuncts();
+        }
+    }
+
     @NotNull
     private PlanFragment getPlanFragmentForPhysicalFileScan(PhysicalFileScan fileScan, PlanTranslatorContext context,
             ScanNode scanNode,
             ExternalTable table, TupleDescriptor tupleDescriptor) {
         scanNode.setNereidsId(fileScan.getId());
-        scanNode.addConjuncts(translateToLegacyConjuncts(fileScan.getConjuncts()));
+        scanNode.addConjuncts(translateToLegacyConjuncts(getConjunctsWithoutPartitionPredicate(fileScan)));
         scanNode.setPushDownAggNoGrouping(context.getRelationPushAggOp(fileScan.getRelationId()));
 
         TableName tableName = new TableName(null, "", "");
@@ -1245,7 +1274,7 @@ public class PhysicalPlanTranslator extends DefaultPlanVisitor<PlanFragment, Pla
             addConjunctsToPlanNode(filter, selectNode, context);
             addPlanRoot(inputFragment, selectNode, filter);
         } else {
-            if (!(filter.child(0) instanceof AbstractPhysicalJoin)) {
+            if (!(filter.child(0) instanceof AbstractPhysicalJoin) && !(planNode instanceof FileQueryScanNode)) {
                 addConjunctsToPlanNode(filter, planNode, context);
                 updateLegacyPlanIdToPhysicalPlan(inputFragment.getPlanRoot(), filter);
             }
