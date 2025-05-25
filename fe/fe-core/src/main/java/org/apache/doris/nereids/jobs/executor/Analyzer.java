aea6c9b3646569b1f9e8c6c94b5482591591a9bc
caiconghui1 <caiconghui1@jd.com>
2025-05-25 09:59:46 +0800

    fix(nereids): do eliminate constant group by key in normalizeagg #49589 (#50212)

--- fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Analyzer.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/jobs/executor/Analyzer.java
@@ -33,7 +33,6 @@ import org.apache.doris.nereids.rules.analysis.CheckPolicy;
 import org.apache.doris.nereids.rules.analysis.CollectJoinConstraint;
 import org.apache.doris.nereids.rules.analysis.CollectSubQueryAlias;
 import org.apache.doris.nereids.rules.analysis.EliminateDistinctConstant;
-import org.apache.doris.nereids.rules.analysis.EliminateGroupByConstant;
 import org.apache.doris.nereids.rules.analysis.EliminateLogicalSelectHint;
 import org.apache.doris.nereids.rules.analysis.FillUpMissingSlots;
 import org.apache.doris.nereids.rules.analysis.HavingToFilter;
@@ -170,8 +169,6 @@ public class Analyzer extends AbstractBatchJobExecutor {
             // select SUM(lo_tax) FROM lineorder group by 1;
             // errCode = 2, detailMessage = GROUP BY expression must not contain aggregate functions: sum(lo_tax)
             bottomUp(new CheckAnalysis()),
-            topDown(new EliminateGroupByConstant()),
-
             topDown(new SimplifyAggGroupBy()),
             // run BuildAggForRandomDistributedTable before NormalizeAggregate in order to optimize the agg plan
             topDown(new BuildAggForRandomDistributedTable()),
