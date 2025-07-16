5571998fb030f8ca0b4bf27b31260f51ab5014b9
weixingyu12 <weixingyu12@jd.com>
2025-07-16 17:17:51 +0800

    feat(cast): keep consistent behavior with presto when cast float/double/decimal to int

--- fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionNormalization.java
+++ fe/fe-core/src/main/java/org/apache/doris/nereids/rules/expression/ExpressionNormalization.java
@@ -21,6 +21,7 @@ import org.apache.doris.nereids.rules.expression.check.CheckCast;
 import org.apache.doris.nereids.rules.expression.rules.BooleanLiteralRewrite;
 import org.apache.doris.nereids.rules.expression.rules.CastBitmapToVarcharRewrite;
 import org.apache.doris.nereids.rules.expression.rules.CastDoubleToDecimalRewrite;
+import org.apache.doris.nereids.rules.expression.rules.CastDoubleToIntRewrite;
 import org.apache.doris.nereids.rules.expression.rules.ConvertAggStateCast;
 import org.apache.doris.nereids.rules.expression.rules.DigitalMaskingConvert;
 import org.apache.doris.nereids.rules.expression.rules.FoldConstantRule;
@@ -63,7 +64,8 @@ public class ExpressionNormalization extends ExpressionRewrite {
                 MergeDateTrunc.INSTANCE,
                 CheckCast.INSTANCE,
                 CastBitmapToVarcharRewrite.INSTANCE,
-                BooleanLiteralRewrite.INSTANCE
+                BooleanLiteralRewrite.INSTANCE,
+                CastDoubleToIntRewrite.INSTANCE
             )
     );
 
